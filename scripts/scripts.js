38600f31f53fd575bc1a64cdc63969b38cdd7bbf
loremlipsum <lipsum.webdevelop@gmail.com>
2019-05-03 14:52:52 +0300

    deploy

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -244,22 +244,22 @@ picturefill();
   document.addEventListener('keyup', onEscKeyup);
 })()
 
-(function($) {
-	'use strict';
-	$(function() {
-
-    var btns = $('.js-show-more-button');
-    var items = '.js-show-more-item-2:nth-child(n+3)';
-
-    if (btns) {
-      btns.on('click', function(e){
-        e.preventDefault();
-        $(this).parents('.js-show-more').find(items).toggleClass('active');
-      });
-    }
-
-	});
-})(jQuery);
+// (function($) {
+// 	'use strict';
+// 	$(function() {
+//
+//     var btns = $('.js-show-more-button');
+//     var items = '.js-show-more-item-2:nth-child(n+3)';
+//
+//     if (btns) {
+//       btns.on('click', function(e){
+//         e.preventDefault();
+//         $(this).parents('.js-show-more').find(items).toggleClass('active');
+//       });
+//     }
+//
+// 	});
+// })(jQuery);
 
 'use strict';
 
